1207f6660dda446a3d60153663c6886fdcd734a0
Adnan Karim <adnan.karim@showpass.com>
2024-09-01 19:14:27 -0600

    updated settings

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y^#=%lxy#+mt4v0kvvperrim1&82h5h4q1w6@z33_4q9+&wtt)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.0.239']
 
 AUTH_USER_MODEL = 'backend.CustomUser'
 
