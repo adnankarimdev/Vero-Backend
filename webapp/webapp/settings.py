62a4d898bf847cc6c19b474e5f78c99d1844d1c8
Adnan Karim <adnan.karim@showpass.com>
2024-09-21 00:55:40 -0600

    postgres local

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -94,13 +94,23 @@ WSGI_APPLICATION = "webapp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vero-test',  # Replace with your database name
+        'USER': 'postgres',  # Replace with your database user
+        'PASSWORD': 'veroadmin',  # Replace with your user's password
+        'HOST': 'localhost',  # Default host for local PostgreSQL
+        'PORT': '5433',  # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
