91e4cf599cd8c31973dd29259fb41cd5f5e283a8
Adnan Karim <adnan.karim@showpass.com>
2024-09-24 11:54:13 -0600

    updated allowess hosts

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = [
     "3.137.148.202",
     "3.134.83.89",
     "52.14.232.28", #t3.medium new ip ec2
+    "ec2-52-14-232-28.us-east-2.compute.amazonaws.com", #t3.medium new web add ec2
     "ec2-3-137-148-202.us-east-2.compute.amazonaws.com",
     "vero-io.com",
     "www.vero-io.com"
