540b067ab4c0f1c7c474077c5ef71b7ecc32b386
Adnan Karim <adnan.karim.dev@gmail.com>
2024-11-06 00:33:42 -0700

    ficed ground breaking bug lol

--- webapp/backend/views.py
+++ webapp/backend/views.py
@@ -1496,7 +1496,7 @@ def save_user_review_question_settings(request):
 
         # Extract place_id from the data
         user_email = data.get("userEmail")
-        user_data = UserData.objects.get(client_email=user_email)
+        user_data = UserData.objects.get(user_email=user_email)
         places = user_data.places_information
         # print("my places", places[0])
         places[0]["offer"] = data.get("complimentaryItem", "")
